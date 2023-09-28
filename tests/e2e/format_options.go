d64a61734fab0d59ecf32f712f2d3030a15af0c8
Drew Sirenko <68304519+AndrewSirenko@users.noreply.github.com>
2023-09-28 11:47:55 -0400

    IN PROG TESTER

---
+++ tests/e2e/format_options.go
@@ -0,0 +1,114 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+	ebscsidriver "github.com/kubernetes-sigs/aws-ebs-csi-driver/pkg/driver"
+	"github.com/kubernetes-sigs/aws-ebs-csi-driver/tests/e2e/driver"
+	"github.com/kubernetes-sigs/aws-ebs-csi-driver/tests/e2e/testsuites"
+	. "github.com/onsi/ginkgo/v2"
+	v1 "k8s.io/api/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/test/e2e/framework"
+	admissionapi "k8s.io/pod-security-admission/api"
+	"strconv"
+)
+
+const (
+	initialVolumeSizeGi     = 2
+	volumeSizeIncreaseAmtGi = 2
+
+	blockSizeTestValue      = "1024"
+	inodeSizeTestValue      = "512"
+	bytesPerInodeTestValue  = "8192"
+	numberOfInodesTestValue = "200192"
+)
+
+// TODO is this a clean place for this?
+var (
+	// TODO having this here is code smell. Hardcode? Test case from original inode PR #1661 https://github.com/kubernetes-sigs/aws-ebs-csi-driver/pull/1661
+	expectedBytesPerInodeTestResult = strconv.Itoa(131072 * initialVolumeSizeGi)
+
+	formatOptionTests = []testsuites.FormatOptionTest{
+		{
+			CreateVolumeParameterKey:        ebscsidriver.BlockSizeKey,
+			CreateVolumeParameterValue:      blockSizeTestValue,
+			ExpectedFilesystemInfoParamName: "Block size",
+			ExpectedFilesystemInfoParamVal:  blockSizeTestValue,
+		},
+		{
+			CreateVolumeParameterKey:        ebscsidriver.INodeSizeKey,
+			CreateVolumeParameterValue:      inodeSizeTestValue,
+			ExpectedFilesystemInfoParamName: "Inode size",
+			ExpectedFilesystemInfoParamVal:  inodeSizeTestValue},
+		{
+			CreateVolumeParameterKey:        ebscsidriver.BytesPerINodeKey,
+			CreateVolumeParameterValue:      bytesPerInodeTestValue,
+			ExpectedFilesystemInfoParamName: "Inode count",
+			ExpectedFilesystemInfoParamVal:  expectedBytesPerInodeTestResult,
+		},
+		{
+			CreateVolumeParameterKey:        ebscsidriver.NumberOfINodesKey,
+			CreateVolumeParameterValue:      numberOfInodesTestValue,
+			ExpectedFilesystemInfoParamName: "Inode count",
+			ExpectedFilesystemInfoParamVal:  numberOfInodesTestValue},
+	}
+)
+
+var _ = Describe("[ebs-csi-e2e] [single-az] [format-options] Formatting a volume", func() {
+	f := framework.NewDefaultFramework("ebs")
+	f.NamespacePodSecurityEnforceLevel = admissionapi.LevelPrivileged // TODO Maybe don't need this if Connor big brain pulls thru
+
+	var (
+		cs        clientset.Interface
+		ns        *v1.Namespace
+		ebsDriver driver.PVTestDriver
+
+		testedFsTypes = []string{ebscsidriver.FSTypeExt4} // TODO Is this right place for this?
+	)
+
+	BeforeEach(func() {
+		cs = f.ClientSet
+		ns = f.Namespace
+		ebsDriver = driver.InitEbsCSIDriver()
+	})
+
+	for _, fsType := range testedFsTypes {
+		Context(fmt.Sprintf("with an %s filesystem", fsType), func() {
+			// TODO: is formatOptionTestCase clear? Or should it be 'formatOptionTestCaseValues'
+			for _, formatOptionTestCase := range formatOptionTests {
+				if fsTypeDoesNotSupportFormatOptionParameter(fsType, formatOptionTestCase.CreateVolumeParameterKey) {
+					continue
+				}
+
+				Context(fmt.Sprintf("with a custom %s parameter", formatOptionTestCase.CreateVolumeParameterKey), func() {
+					It("successfully mounts and is resizable", func() {
+						test := testsuites.FormatOptionTest{
+							CSIDriver:                   ebsDriver,
+							testsuites.FormatOptionTest: formatOptionTestCase,
+						}
+						test.Run(cs, ns)
+					})
+				})
+			}
+		})
+	}
+})
+
+func fsTypeDoesNotSupportFormatOptionParameter(fsType string, createVolumeParameterKey string) bool {
+	_, paramNotSupported := ebscsidriver.FileSystemConfigs[fsType].NotSupportedParams[createVolumeParameterKey]
+	return paramNotSupported
+}
---
+++ tests/e2e/testsuites/format_options_tester.go
@@ -0,0 +1,124 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testsuites
+
+import (
+	"fmt"
+	awscloud "github.com/kubernetes-sigs/aws-ebs-csi-driver/pkg/cloud"
+	"github.com/onsi/gomega/format"
+	v1 "k8s.io/api/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/test/e2e/framework"
+	"regexp"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+// FormatOptionTest will provision required StorageClass(es), PVC(s) and Pod(s) TODO
+// Waiting for the PV provisioner to create a new PV
+// Update pvc storage size
+// Waiting for new PVC and PV to be ready
+// And finally attach pvc to the pod and wait for pod to be ready.
+type FormatOptionTest struct {
+	CreateVolumeParameterKey        string
+	CreateVolumeParameterValue      string
+	ExpectedFilesystemInfoParamName string
+	ExpectedFilesystemInfoParamVal  string
+}
+
+const (
+	volumeSizeIncreaseAmtGi = 2
+
+	volumeMountPath = "/mnt/test-format-option" // TODO should I keep this as mnt/test-1, and refactor to be `DefaultMountPath` globally in testsuites?
+)
+
+var (
+	podCmdGetFsInfo     = fmt.Sprintf("tune2fs -l $(df -k '%s'| tail -1 | awk '{ print $1 }')", volumeMountPath)                               // Gets the filesystem info for the mounted volume
+	podCmdWriteToVolume = fmt.Sprintf("echo 'hello world' >> %s/data && grep 'hello world' %s/data && sync", volumeMountPath, volumeMountPath) // TODO Debt: All the dynamic provisioning tests use this same cmd. Should we refactor out into exported constant?
+
+)
+
+func (t *FormatOptionTest) Run(client clientset.Interface, namespace *v1.Namespace, fsType string) {
+	By("setting up pvc")
+	volumeDetails := createFormatOptionVolumeDetails(fsType, volumeMountPath, t)
+	testPvc, _ := volumeDetails.SetupDynamicPersistentVolumeClaim(client, namespace, ebsDriver)
+	defer testPvc.Cleanup()
+
+	By("deploying pod with custom format option")
+	getFsInfoTestPod := createPodWithVolume(client, namespace, podCmdGetFsInfo, testPvc, volumeDetails)
+	defer getFsInfoTestPod.Cleanup()
+	getFsInfoTestPod.WaitForSuccess() // TODO e2e test implementation defaults to a 15 min wait instead of 5 min one... Is that fine or refactor worthy?
+
+	By("confirming custom format option was applied")
+	fsInfoSearchRegexp := fmt.Sprintf(`%s:\s+%s`, t.ExpectedFilesystemInfoParamName, t.ExpectedFilesystemInfoParamVal)
+	if isFormatOptionApplied := FindRegexpInPodLogs(fsInfoSearchRegexp, getFsInfoTestPod); !isFormatOptionApplied {
+		framework.Failf("Did not find expected %s value of %s in filesystem info", t.ExpectedFilesystemInfoParamName, t.ExpectedFilesystemInfoParamVal)
+	}
+
+	By("testing that pvc is able to be resized")
+	ResizeTestPvc(client, namespace, testPvc, volumeSizeIncreaseAmtGi)
+
+	By("validating resized pvc by deploying new pod")
+	resizeTestPod := createPodWithVolume(client, namespace, podCmdWriteToVolume, testPvc, volumeDetails)
+	defer resizeTestPod.Cleanup()
+
+	By("confirming new pod can write to resized volume")
+	resizeTestPod.WaitForSuccess()
+}
+
+// TODO should we improve this across e2e tests via builder design pattern? Or is that not go-like?
+func createFormatOptionVolumeDetails(fsType string, volumeMountPath string, t FormatOptionTest) *VolumeDetails {
+	allowVolumeExpansion := true
+
+	volume := VolumeDetails{
+		VolumeType:   awscloud.VolumeTypeGP2,
+		FSType:       fsType,
+		MountOptions: []string{"rw"},
+		ClaimSize:    fmt.Sprintf("%vGi", initialVolumeSizeGi),
+		VolumeMount: VolumeMountDetails{
+			NameGenerate:      "test-volume-format-option",
+			MountPathGenerate: volumeMountPath,
+		},
+		AllowVolumeExpansion: &allowVolumeExpansion,
+		AdditionalParameters: map[string]string{
+			t.CreateVolumeParameterKey: t.CreateVolumeParameterValue,
+		},
+	}
+
+	return &volume
+}
+
+// TODO putting this in function may be overkill? In an ideal world we refactor out TestEverything objects so testPod.SetupVolume isn't gross.
+func createPodWithVolume(client clientset.Interface, namespace *v1.Namespace, cmd string, testPvc *TestPersistentVolumeClaim, volumeDetails *VolumeDetails) *TestPod {
+	testPod := NewTestPod(client, namespace, cmd)
+	testPod.SetupVolume(testPvc.persistentVolumeClaim, volumeDetails.VolumeMount.NameGenerate, volumeDetails.VolumeMount.MountPathGenerate, volumeDetails.VolumeMount.ReadOnly)
+	testPod.Create()
+
+	return testPod
+}
+
+// TODO: Maybe should use something other than Find(), but *shrug*
+// TODO should I move this to go ?
+// TODO Should I instead use RunHostCmd or LookForString from https://github.com/kubernetes/kubernetes/blob/master/test/e2e/framework/pod/output/output.go ?
+
+// FindRegexpInPodLogs searches given testPod's logs for a given regular expression. Returns `true` if found.
+func FindRegexpInPodLogs(regexpPattern string, testPod *TestPod) bool {
+	podLogs, err := testPod.Logs()
+	framework.ExpectNoError(err, "Tried getting logs for pod %s", format.Object(testPod, 2))
+
+	var expectedLine = regexp.MustCompile(regexpPattern)
+	res := expectedLine.Find(podLogs)
+	return res != nil
+}
